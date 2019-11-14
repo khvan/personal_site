a1190726c26cb8dbe9e59494f68b88b92a1bff55
kBoris <khvanboris@gmail.com>
2019-11-14 13:12:11 -0800

    basic setup of React app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Welcome My React App
+      </div>
+    )
+  }
+}
